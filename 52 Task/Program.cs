b30f5570cab4f3a9fe2cfaa039338a0dcd1c0dfd
Ulia Kamalova <mysula@mail.ru>
2022-09-01 14:36:03 +0500

    52

---
+++ 52 Task/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+int m = ReadInt("Задайте количество строк: ");
+int n = ReadInt("Задайте количество столбцов: ");
+int[,] matrix = new int[m, n];
+
+int sum = 0;
+
+FillArray(matrix);
+PrintArray(matrix);
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = sum + matrix[i, j];
+    }
+    double average = Math.Round((double)sum / m * 1, 1);
+    Console.Write($"{average}\t");
+    sum = 0;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
